1c8054f5047624da9cc1525a0fce0fb0890a7a84
JORAM KIRUBI KIMANI <Joramkirubi100@gmail.com>
2025-09-07 20:15:19 +0300

    Fix query_samples.py with correct queries for author and librarian

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -13,5 +13,5 @@ def books_in_library(library_name):
 # Query 3: Retrieve the librarian for a library
 def librarian_of_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.librarian
+    return Librarian.objects.get(library=library)
 
