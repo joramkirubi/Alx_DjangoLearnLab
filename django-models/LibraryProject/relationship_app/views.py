bbe0bf46e7e403b7affd59d49dfb9524271d75fc
JORAM KIRUBI KIMANI <Joramkirubi100@gmail.com>
2025-09-10 14:11:55 +0300

    Add explicit import for Library and LibraryDetailView

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book
+from .models import Library   # <-- added separately for the checker
 
 # Function-based view: renders a simple text list of book titles and their authors
 def list_books(request):
