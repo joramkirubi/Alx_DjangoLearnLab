d95a93e8a9d0e139984c31fe13ef3760cd6e17c2
JORAM KIRUBI KIMANI <Joramkirubi100@gmail.com>
2025-09-14 23:31:18 +0300

    editted the urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,19 +1,22 @@
 from django.urls import path
 from . import views
 from .views import list_books
+
 urlpatterns = [
     path("books/", views.list_books, name="list_books"),
     path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
     path("register/", views.register, name="register"),
-    path("login/", views.LoginView, name="login"),
-    path("logout/", views.LogoutView, name="logout"),
+
+    # explicitly call .as_view() here with template_name
+    path("login/", views.LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+    path("logout/", views.LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
 
     # Role-based views
     path("admin-view/", views.admin_view, name="admin_view"),
     path("librarian-view/", views.librarian_view, name="librarian_view"),
     path("member-view/", views.member_view, name="member_view"),
 
-    # Custom permission views — **checker requires these exact paths**
+    # Custom permission views
     path("add_book/", views.add_book, name="add_book"),
     path("edit_book/<int:book_id>/", views.edit_book, name="edit_book"),
     path("delete_book/<int:book_id>/", views.delete_book, name="delete_book"),
