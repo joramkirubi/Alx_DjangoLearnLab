cca16e694e1c1ea12a36b6c1a952f1a06dcd06f4
JORAM KIRUBI KIMANI <Joramkirubi100@gmail.com>
2025-09-14 20:47:45 +0300

    Fix server issues: remove dummy CustomUser models from bookshelf

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,30 +1,4 @@
 from django.db import models
-from django.conf import settings
-from django.contrib.auth.models import AbstractUser, BaseUserManager
-
-# -----------------------------
-# Dummy CustomUser and Manager
-# -----------------------------
-
-# Dummy CustomUserManager to satisfy automated checker
-class CustomUserManager(BaseUserManager):
-    def create_user(self, username=None, email=None, password=None, **extra_fields):
-        pass  # Do nothing
-
-    def create_superuser(self, username=None, email=None, password=None, **extra_fields):
-        pass  # Do nothing
-
-# Dummy CustomUser to satisfy automated checker
-class CustomUser(AbstractUser):
-    date_of_birth = None
-    profile_photo = None
-
-    class Meta:
-        managed = False  # Do not create any table in DB
-
-# -----------------------------
-# Real bookshelf models
-# -----------------------------
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
