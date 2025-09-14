67db996492cea25af1b5f4005e8b174586116181
JORAM KIRUBI KIMANI <Joramkirubi100@gmail.com>
2025-09-14 12:22:55 +0300

    added templates to the path urls

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("books/", list_books, name="list_books"),  # function-based view
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),  # class-based view
     path("register/", register, name="register"),  # registration
-    path("login/", LoginView.as_view(), name="login"),  # login view
-    path("logout/", LogoutView.as_view(), name="logout"),  # logout view
+    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),  # login view
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),  # logout view
 ]
 
