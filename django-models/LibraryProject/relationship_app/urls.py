277a5bdd80d614b8f9882905cbddf25a43c3ca8a
JORAM KIRUBI KIMANI <Joramkirubi100@gmail.com>
2025-09-14 13:37:18 +0300

    minor code

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,16 +1,15 @@
 from django.urls import path
-from . import views   
+from . import views
 from .views import list_books, LibraryDetailView
-from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path("books/", list_books, name="list_books"),  # function-based view
-    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),  # class-based view
-    path("register/", views.register, name="register"),  # registration
-    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),  # login view
-    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),  # logout view
+    path("books/", views.list_books, name="list_books"),
+    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
+    path("register/", views.register, name="register"),
+    path("login/", views.LoginView, name="login"),   # use the alias from views.py
+    path("logout/", views.LogoutView, name="logout"),  # same here
 
-     # role-based views
+    # role-based views
     path("admin-view/", views.admin_view, name="admin_view"),
     path("librarian-view/", views.librarian_view, name="librarian_view"),
     path("member-view/", views.member_view, name="member_view"),
