c0b3a3339429d4e6246b01fe15f97823764e130a
JORAM KIRUBI KIMANI <Joramkirubi100@gmail.com>
2025-09-14 12:15:38 +0300

    connected the backend to the front end

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, register, LoginView, LogoutView
+from .views import list_books, LibraryDetailView, register
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path("books/", list_books, name="list_books"),
-    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
-    path("register/", register, name="register"),
-    path("login/", LoginView, name="login"),
-    path("logout/", LogoutView, name="logout"),
+    path("books/", list_books, name="list_books"),  # function-based view
+    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),  # class-based view
+    path("register/", register, name="register"),  # registration
+    path("login/", LoginView.as_view(), name="login"),  # login view
+    path("logout/", LogoutView.as_view(), name="logout"),  # logout view
 ]
+
