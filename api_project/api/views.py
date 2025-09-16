b9edce3b284154288e3d5b2829c8d20358124461
JORAM KIRUBI KIMANI <Joramkirubi100@gmail.com>
2025-09-16 14:57:36 +0300

    Implemented authentication and permissions in DRF

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .models import Book
 from .serializers import BookSerializer
 
@@ -7,10 +8,12 @@ from .serializers import BookSerializer
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # 👈 Only logged-in users can list books
 
 
 # New ViewSet (full CRUD)
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAdminUser]  # 👈 Only admins can CRUD
 
